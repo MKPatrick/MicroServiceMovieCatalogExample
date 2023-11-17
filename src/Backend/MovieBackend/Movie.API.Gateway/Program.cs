7f4045dee3bbf5ddcd91c27732e83e1c10be6538
Patrick Meier <104093287+MKPatrick@users.noreply.github.com>
2023-11-17 12:07:25 +0100

    setup ocelot api gateway

--- src/Backend/MovieBackend/Movie.API.Gateway/Program.cs
+++ src/Backend/MovieBackend/Movie.API.Gateway/Program.cs
@@ -1,6 +1,10 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("Configuration/ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +22,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
-
+await app.UseOcelot();
 app.Run();
+
